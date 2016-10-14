15dd2a1d7c85910e1b79ebaa057908a66f387638
SufferProgrammer <pijipirma@gmail.com>
2016-10-14 14:14:53 +0700

    fix command not found on pip

--- install-openedX.sh
+++ install-openedX.sh
@@ -2,6 +2,7 @@
 
 echo '[*]Installing system requirements please wait...'
 apt-get update
+
 apt-get install python python-dev python-setuptools build-essential libmysqlclient-dev
 easy_install pip==8.1.2
 pip install --upgrade pip
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo '[*]Starting Open edX application...'
 
-cd /var/tmp/configuration/playbooks/ && ansible-playbook -c local ./edx_sandbox.yml -i "localhost,"
+cd /var/tmp/configuration/playbooks/ && ansible-playbook -c local ./edx_sandbox.yml -i "127.0.0.1,"
