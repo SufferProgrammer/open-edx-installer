9f17d655b32b68d8f5910b809f7f2499f0b33310
SufferProgrammer <pijipirma@gmail.com>
2016-10-14 15:17:16 +0700

    modified a little bit stuff

--- install-openedX.sh
+++ install-openedX.sh
@@ -10,6 +10,6 @@ apt-get update --fix-missing
 easy_install pip==8.1.2
 pip install --upgrade pip
 cd package
-pip install -r requrements.txt
+pip install -r requirements.txt
 
 echo '[*]Installing system done please start run.sh for running application...'
